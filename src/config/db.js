43cfe2424ee07ab7ad7863bd93d17e29261e20b5
wabi <wabi-lat@epitech.eu>
2025-05-16 10:11:14 +0200

    feat: fix the db.js config

--- src/config/db.js
+++ src/config/db.js
@@ -1,28 +1,27 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
-const { setConnection } = require('./const_db.js');
+const { setConnection } = require("./const_db.js");
 
-function create_logs(callback, connection) {
-    const createLogsTable = `
-        CREATE TABLE IF NOT EXISTS logs (
+function create_user(callback, connection) {
+const createUserTable = `
+        CREATE TABLE IF NOT EXISTS user (
         id INT AUTO_INCREMENT PRIMARY KEY,
-        email VARCHAR(100) NOT NULL,
+        email VARCHAR(100) NOT NULL UNIQUE,
         password VARCHAR(100) NOT NULL,
-        last_name VARCHAR(100) NOT NULL,
-        first_name VARCHAR(100) NOT NULL,
-        day_time VARCHAR(100) NOT NULL
+        name VARCHAR(100) NOT NULL,
+        firstname VARCHAR(100) NOT NULL,
+        created_at DATETIME DEFAULT CURRENT_TIMESTAMP
         )`;
-    connection.query(createLogsTable, (err) => {
-        if (err)
-            return callback(err);
-        console.log("PASS TABLE LOG");
-        callback(null, connection);
-    });
+connection.query(createUserTable, (err) => {
+    if (err) return callback(err);
+    console.log("PASS TABLE LOG");
+    callback(null, connection);
+});
 }
 
 function create_todo(callback, connection) {
-    const createTodoTable = `
+const createTodoTable = `
         CREATE TABLE IF NOT EXISTS todo (
         id INT AUTO_INCREMENT PRIMARY KEY,
         title VARCHAR(100) NOT NULL,
@@ -31,54 +30,56 @@ function create_todo(callback, connection) {
         due_time DATETIME NOT NULL, 
         status enum('not started', 'to do', 'in progress', 'done') NOT NULL,
         user_id INT NOT NULL,
-        FOREIGN KEY (user_id) REFERENCES logs(id)
+        FOREIGN KEY (user_id) REFERENCES user(id)
         )`;
-    connection.query(createTodoTable, (err) => {
-    if (err)
-        return callback(err);
+connection.query(createTodoTable, (err) => {
+    if (err) return callback(err);
     console.log("PASS TABLE TO DO0");
     callback(null, connection);
-    });
+});
 }
 
 function initDatabase(callback) {
-    const connection = mysql.createConnection({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-    });
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+});
 
-    connection.query(`CREATE DATABASE IF NOT EXISTS \`${process.env.DB_NAME}\``, (err) => {
+connection.query(
+    `CREATE DATABASE IF NOT EXISTS \`${process.env.DB_NAME}\``,
+    (err) => {
+    if (err) {
+        console.log("[ERROR] - Creating the DataBase");
+        return callback(err);
+    }
+    connection.changeUser({ database: process.env.DB_NAME }, (err) => {
         if (err) {
-            console.log("[ERROR] - Creating the DataBase");
+        console.log("[ERROR] - Changing the User");
+        return callback(err);
+        }
+        create_user((err) => {
+        if (err) {
+            console.log("[ERROR] - Creating the user");
             return callback(err);
         }
-        connection.changeUser({ database: process.env.DB_NAME }, (err) => {
-            if (err) {
-                console.log("[ERROR] - Changing the User");
-                return callback(err);
-            }
-            create_logs((err) => {
-            if (err) {
-                console.log("[ERROR] - Creating the Logs");
-                return callback(err);
-            }
-            create_todo((err) => {
-                if (err) return callback(err);
-                    setConnection(connection);
-                    callback(null, connection);
-                }, connection);
-            }, connection);
-        });
+        create_todo((err) => {
+            if (err) return callback(err);
+            setConnection(connection);
+            callback(null, connection);
+        }, connection);
+        }, connection);
     });
+    }
+);
 }
 
 module.exports = {
-    initDatabase: initDatabase,
-    connection: mysql.createConnection({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-    }),
+initDatabase: initDatabase,
+connection: mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+}),
 };
